2d5a62be9f546a6f9132fbb1ba5765b068a7a185
Ave M <merritt.ave@gmail.com>
2021-04-21 16:54:33 -0700

    Commit 4/21/2021

---
+++ Spatial1.R
@@ -0,0 +1,31 @@
+library(rsyncrosim)
+library(raster)
+
+stratumTif <- "C:/Users/merri/Documents/CDL Research/SyncroSim in R/OUTPUTS/bps_aoi_crop.tif"
+sclassTif <- "C:/Users/merri/Documents/CDL Research/SyncroSim in R/OUTPUTS/sclass_aoi_crop.tif"
+
+rStratum <- raster(stratumTif)
+rSclass <-raster(sclassTif)
+
+myLibrary <- ssimLibrary(name = "NEIDMOF.ssim", package = "landfirevegmodels", overwrite = F)
+project(myLibrary)
+
+myProject = project(myLibrary, project = "Definitions")
+myProject
+
+datasheet(myProject, summary = T)
+scenario(myProject, summary = T)
+
+myScenario = scenario(myProject, scenario = "Modern Percentages spatial")
+myScenario
+
+sheetName <- "stsim_InitialConditionsSpatial"
+sheetData <- list(StratumFileName = stratumTif, 
+                  StateClassFileName = sclassTif)
+saveDatasheet(myScenario, sheetData, sheetName)
+
+rStratumTest <- datasheetRaster(myScenario, sheetName, "StratumFileName")
+rSclassTest <- datasheetRaster(myScenario, sheetName, "StateClassFileName")
+
+plot(rStratumTest)
+plot(rSclassTest)
